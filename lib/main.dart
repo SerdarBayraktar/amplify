e563646fdfbe81aa4e8849620eaab080621ff125
Serdar Bayraktar <serdarbayraktarmath@gmail.com>
2022-05-22 00:50:44 +0300

    Project files splitted and todo app creted

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,207 @@
-import 'package:amplify_datastore/amplify_datastore.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
+// dart async library we will refer to when setting up real time updates
+import 'dart:async';
+// flutter and ui libraries
 import 'package:flutter/material.dart';
-
+// amplify packages we will need to use
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+// amplify configuration and models that should have been generated for you
 import 'amplifyconfiguration.dart';
 import 'models/ModelProvider.dart';
+import 'models/Todo.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Amplified Todo',
+      home: TodosPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
+class TodosPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TodosPageState createState() => _TodosPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  bool _amplifyConfigured = false;
+class _TodosPageState extends State<TodosPage> {
+
+  bool _isLoading = true;
+  List<Todo> _todos = [];
+  final AmplifyDataStore _dataStorePlugin =
+  AmplifyDataStore(modelProvider: ModelProvider.instance);
+  late StreamSubscription<QuerySnapshot<Todo>> _subscription;
 
   @override
   void initState() {
+    _initializeApp();
     super.initState();
-    _configureAmplify();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> _initializeApp() async {
+    await _configureAmplify();
+
+    _subscription = Amplify.DataStore.observeQuery(Todo.classType)
+        .listen((QuerySnapshot<Todo> snapshot) {
+      setState(() {
+        if (_isLoading) _isLoading = false;
+        _todos = snapshot.items;
+      });
+    });
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      await Amplify.addPlugins([_dataStorePlugin]);
+      await Amplify.configure(amplifyconfig);
+    } catch (e) {
+      print('An error occurred while configuring Amplify: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('My Todo List'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'asd',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+       body: _isLoading
+             ? Center(child: CircularProgressIndicator())
+           : TodosList(todos: _todos),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTodoForm()),
+          );
+        },
+        tooltip: 'Add Todo',
+        label: Row(
+          children: [Icon(Icons.add), Text('Add todo')],
         ),
-      )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-  void _configureAmplify() async {
-    // await Amplify.addPlugin(AmplifyAPI()); // UNCOMMENT this line after backend is deployed
-    await Amplify.addPlugin(AmplifyDataStore(modelProvider: ModelProvider.instance));
-    await Amplify.configure(amplifyconfig);
+}
+
+class TodosList extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosList({required this.todos});
+
+  @override
+  Widget build(BuildContext context) {
+    return todos.length >= 1
+        ? ListView(
+        padding: EdgeInsets.all(8),
+        children: todos.map((todo) => TodoItem(todo: todo)).toList())
+        : Center(child: Text('Tap button below to add a todo!'));
+  }
+}
+
+class TodoItem extends StatelessWidget {
+  final double iconSize = 24.0;
+  final Todo todo;
+
+  TodoItem({required this.todo});
+
+  void _deleteTodo(BuildContext context) async {
+    // to be filled in a later step
+  }
+
+  Future<void> _toggleIsComplete() async {
+    Todo updatedTodo = todo.copyWith(isComplete: !todo.isComplete);
     try {
-      setState(() {
-        _amplifyConfigured = true;
-      });
+      await Amplify.DataStore.save(updatedTodo);
     } catch (e) {
-      print(e);
+      print('An error occurred while saving Todo: $e');
     }
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _toggleIsComplete();
+        },
+        onLongPress: () {
+          _deleteTodo(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(todo.name,
+                      style:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Text(todo.description ?? 'No description'),
+                ],
+              ),
+            ),
+            Icon(
+                todo.isComplete
+                    ? Icons.check_box
+                    : Icons.check_box_outline_blank,
+                size: iconSize),
+          ]),
+        ),
+      ),
+    );
+  }
+}
 
+class AddTodoForm extends StatefulWidget {
+  @override
+  _AddTodoFormState createState() => _AddTodoFormState();
+}
 
+class _AddTodoFormState extends State<AddTodoForm> {
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
 
+  Future<void> _saveTodo() async {
+    // to be filled in a later step
+  }
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Todo'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(filled: true, labelText: 'Name')),
+              TextFormField(
+                  controller: _descriptionController,
+                  decoration:
+                  InputDecoration(filled: true, labelText: 'Description')),
+              ElevatedButton(onPressed: _saveTodo, child: Text('Save'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
